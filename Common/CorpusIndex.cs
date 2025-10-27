dd516f8054c731399ddbe7a527bf95b7d704d5c5
Aceius E. <aceiuselms@seattleacademy.org>
2025-10-27 14:27:55 -0700

    Index creation

---
+++ Common/CorpusIndex.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+namespace Common;
+
+public class CorpusIndex
+{
+    public string[] Terms { get; set; } = [];
+    public List<int[]> Documents { get; set; } = [];
+
+    public void ToFile(string path) => File.WriteAllText(path, JsonSerializer.Serialize(this));
+}
--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
+using Common;
 
 namespace Indexer;
 
@@ -17,28 +18,6 @@ internal partial class Program
         .Select(word => PorterStemmer.StemOneWord(word, new PorterStemmerImpl()))
         .ToList();
 
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Starting Indexer");
-        Stopwatch timer = Stopwatch.StartNew();
-
-        var articles = Parser.ParseFile("SmallWiki.xml");
-        timer.Report("Corpus Parsed");
-
-        List<string> allTerms = articles.Select(GetAllTerms).SelectMany(i => i).Distinct().ToList();
-        timer.Report("Terms Tokenized & Stemmed");
-
-        foreach (Article article in articles)
-        {
-            var l = TermOccurrence(GetAllTerms(article));
-        }
-        
-        Dictionary<string, int> termOccurrence = TermOccurrence(allTerms);
-        timer.Report("Term Occurrence Counted");
-        
-        
-    }
-
     private static Dictionary<string, int> TermOccurrence(List<string> terms)
     {
         Dictionary<string, int> termOccurrences = [];
@@ -49,5 +28,49 @@ internal partial class Program
         }
 
         return termOccurrences;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Indexer");
+        Stopwatch timer = Stopwatch.StartNew();
+        Stopwatch fullTimer = Stopwatch.StartNew();
+
+        var articles = Parser.ParseFile("SmallWiki.xml");
+        timer.Report("Articles Parsed");
+
+        List<string> allTerms = articles.Select(GetAllTerms).SelectMany(i => i).ToList();
+        timer.Report("Terms Tokenized & Stemmed");
+
+        // All unique terms
+        string[] corpus = allTerms.Distinct().ToArray();
+        List<int[]> documentVectors = [];
+
+        // The following could be a linq statement but whatever
+        foreach (List<string> articleTerms in articles.Select(GetAllTerms))
+        {
+            List<int> corpusTermCount = [];
+            Dictionary<string, int> articleTermCount = TermOccurrence(articleTerms);
+
+            foreach (string term in corpus)
+            {
+                corpusTermCount.Add(articleTermCount.GetValueOrDefault(term, 0));
+            }
+
+            documentVectors.Add(corpusTermCount.ToArray());
+        }
+        
+        timer.Report("Document Vectors Created");
+
+        CorpusIndex index = new CorpusIndex()
+        {
+            Terms = corpus,
+            Documents = documentVectors.ToList()
+        };
+
+        index.ToFile(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "index.json"));
+        timer.Report("Index File Written");
+        
+        Console.WriteLine($"Done in {fullTimer.Elapsed}");
     }
 }
