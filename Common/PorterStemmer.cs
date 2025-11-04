4e88d6944089ad678be7223acb8d655959b7ecd0
Aceius E. <aceiuselms@seattleacademy.org>
2025-11-04 14:24:52 -0800

    Rewrite porter stemmer

--- Common/PorterStemmer.cs
+++ Common/PorterStemmer.cs
@@ -1,312 +1,297 @@
-namespace Indexer;
+using System.Text.RegularExpressions;
+namespace Common;
 
-// C# port of provided scala code
-
-/// <summary>
-/// Provides a contract for Porter Stemmer (and other Stemmers).
-/// Serves as an API.
-/// </summary>
-public interface IStemmer
+public class PorterStemmer
 {
+    private static readonly PorterStemmer Instance = new();
+
+    public static string[] StemWords(string[] input) => Instance.StemArray(input);
+
     /// <summary>
     /// Stems an array of words
     /// </summary>
     /// <param name="input">the array of words to be stemmed</param>
     /// <returns>an array containing the stemmed words</returns>
-    string[] StemArray(string[] input);
+    public string[] StemArray(string[] input)
+    {
+        string[] result = new string[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            result[i] = StemWord(input[i]);
+        }
+
+        return result;
+    }
 
     /// <summary>
     /// Stems a string of words separated by spaces
     /// </summary>
     /// <param name="input">the string of words to be stemmed</param>
     /// <returns>a string containing the stemmed words separated by spaces</returns>
-    string Stem(string input);
-}
-
-/// <summary>
-/// Porter stemmer in C# (converted from Scala).
-/// Original paper:
-/// Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14, no. 3, pp 130–137
-/// See also http://www.tartarus.org/~martin/PorterStemmer
-/// </summary>
-public class PorterStemmer : IStemmer
-{
-    /// <summary>
-    /// Stems one word using a given stemmer instance.
-    /// </summary>
-    public static string StemOneWord(string input, PorterStemmerImpl stemmer)
-    {
-        stemmer.Add(input.Trim());
-
-        if (stemmer.Word.Length > 2)
-        {
-            stemmer.Step1();
-            stemmer.Step2();
-            stemmer.Step3();
-            stemmer.Step4();
-            stemmer.Step5a();
-            stemmer.Step5b();
-        }
-
-        return stemmer.Word;
-    }
-
     public string Stem(string input)
     {
-        var stemmer = new PorterStemmerImpl();
-        string[] wordList = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        var result = new List<string>();
+        if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+        string[] words = input.Split([
+            ' ', '\t', '\r', '\n'
+        ], StringSplitOptions.RemoveEmptyEntries);
+        string[] stemmedWords = StemArray(words);
+        return string.Join(" ", stemmedWords);
+    }
 
-        foreach (var word in wordList)
+    // === Core Porter Algorithm ===
+
+    private string StemWord(string word)
+    {
+        if (word.Length < 3)
+            return word.ToLowerInvariant();
+
+        word = word.ToLowerInvariant();
+
+        word = Step1a(word);
+        word = Step1b(word);
+        word = Step1c(word);
+        word = Step2(word);
+        word = Step3(word);
+        word = Step4(word);
+        word = Step5a(word);
+        word = Step5b(word);
+
+        return word;
+    }
+
+    private static readonly Regex MGr0 = new(@"[aeiouy].*[^aeiou][aeiouy]", RegexOptions.Compiled);
+    private static readonly Regex MEq1 = new(@"^[^aeiou]*[aeiouy][^aeiouy]*$", RegexOptions.Compiled);
+    private static readonly Regex MGr1 = new(@"^[^aeiou]*([aeiouy][^aeiouy]*){2,}", RegexOptions.Compiled);
+    private static readonly Regex VowelInStem = new(@"[aeiouy]", RegexOptions.Compiled);
+
+    private static readonly Regex DoubleConsonant = new(@"([^aeiou])\1$", RegexOptions.Compiled);
+    private static readonly Regex Cvc = new(@"[^aeiou][aeiouy][^aeiouywx]$", RegexOptions.Compiled);
+
+    private string Step1a(string word)
+    {
+        if (word.EndsWith("sses")) return word[..^2];
+        if (word.EndsWith("ies")) return word[..^2];
+        if (word.EndsWith("ss")) return word;
+        if (word.EndsWith("s")) return word[..^1];
+        return word;
+    }
+
+    private string Step1b(string word)
+    {
+        bool flag = false;
+        if (word.EndsWith("eed"))
         {
-            stemmer.Add(word.Trim());
-
-            if (stemmer.Word.Length > 2)
+            var stem = word[..^3];
+            if (MGr0.IsMatch(stem))
+                return stem + "ee";
+        }
+        else if (word.EndsWith("ed"))
+        {
+            var stem = word[..^2];
+            if (VowelInStem.IsMatch(stem))
             {
-                stemmer.Step1();
-                stemmer.Step2();
-                stemmer.Step3();
-                stemmer.Step4();
-                stemmer.Step5a();
+                word = stem;
+                flag = true;
             }
-
-            result.Add(stemmer.Word);
         }
-
-        return string.Join(" ", result);
-    }
-
-    public string[] StemArray(string[] input)
-    {
-        var s = new PorterStemmerImpl();
-        string[] result = new string[input.Length];
-        for (int i = 0; i < input.Length; i++)
+        else if (word.EndsWith("ing"))
         {
-            result[i] = StemOneWord(input[i], s);
-        }
-        return result;
-    }
-}
-
-public class PorterStemmerImpl
-{
-    public string Word { get; set; } = "";
-
-    private bool Cons(int i)
-    {
-        char ch = Word[i];
-        string vowels = "aeiou";
-
-        if (vowels.Contains(ch))
-            return false;
-
-        if (ch == 'y')
-        {
-            if (i == 0) return true;
-            return !Cons(i - 1);
-        }
-
-        return true;
-    }
-
-    public void Add(char ch)
-    {
-        Word += ch;
-    }
-
-    public void Add(string newWord)
-    {
-        Word = newWord;
-    }
-
-    public int CalcM(string s)
-    {
-        int count = 0;
-        bool currentConst = false;
-
-        for (int c = 0; c < s.Length; c++)
-        {
-            if (Cons(c))
+            var stem = word[..^3];
+            if (VowelInStem.IsMatch(stem))
             {
-                if (!currentConst && c != 0)
-                    count++;
-                currentConst = true;
-            }
-            else
-            {
-                currentConst = false;
+                word = stem;
+                flag = true;
             }
         }
 
-        return count;
-    }
-
-    public bool VowelInStem(string s)
-    {
-        for (int i = 0; i < Word.Length - s.Length; i++)
+        if (flag)
         {
-            if (!Cons(i))
-                return true;
+            if (word.EndsWith("at") || word.EndsWith("bl") || word.EndsWith("iz"))
+                word += "e";
+            else if (DoubleConsonant.IsMatch(word) && !word.EndsWith("l") && !word.EndsWith("s") && !word.EndsWith("z"))
+                word = word[..^1];
+            else if (MEq1.IsMatch(word) && Cvc.IsMatch(word))
+                word += "e";
         }
-        return false;
+
+        return word;
     }
 
-    public bool DoubleC()
+    private string Step1c(string word)
     {
-        int i = Word.Length - 1;
-        if (i < 1) return false;
-        if (Word[i] != Word[i - 1]) return false;
-        return Cons(i);
-    }
-
-    public bool Cvc(string s)
-    {
-        int i = Word.Length - 1 - s.Length;
-        if (i < 2 || !Cons(i) || Cons(i - 1) || !Cons(i - 2))
-            return false;
-
-        char ch = Word[i];
-        string vals = "wxy";
-        return !vals.Contains(ch);
-    }
-
-    public bool Replacer(string orig, string replace, Func<int, bool> checker)
-    {
-        int l = Word.Length;
-        int origLength = orig.Length;
-        bool res = false;
-
-        if (Word.EndsWith(orig))
+        if (word.EndsWith("y"))
         {
-            string n = Word.Substring(0, l - origLength);
-            int m = CalcM(n);
+            var stem = word[..^1];
+            if (VowelInStem.IsMatch(stem))
+                return stem + "i";
+        }
+        return word;
+    }
 
-            if (checker(m))
+    private string Step2(string word)
+    {
+        string[,] suffixes =
+        {
             {
-                Word = n + replace;
+                "ational", "ate"
+            },
+            {
+                "tional", "tion"
+            },
+            {
+                "enci", "ence"
+            },
+            {
+                "anci", "ance"
+            },
+            {
+                "izer", "ize"
+            },
+            {
+                "abli", "able"
+            },
+            {
+                "alli", "al"
+            },
+            {
+                "entli", "ent"
+            },
+            {
+                "eli", "e"
+            },
+            {
+                "ousli", "ous"
+            },
+            {
+                "ization", "ize"
+            },
+            {
+                "ation", "ate"
+            },
+            {
+                "ator", "ate"
+            },
+            {
+                "alism", "al"
+            },
+            {
+                "iveness", "ive"
+            },
+            {
+                "fulness", "ful"
+            },
+            {
+                "ousness", "ous"
+            },
+            {
+                "aliti", "al"
+            },
+            {
+                "iviti", "ive"
+            },
+            {
+                "biliti", "ble"
             }
-            res = true;
-        }
-
-        return res;
-    }
-
-    public bool ProcessSubList(List<(string, string)> list, Func<int, bool> checker)
-    {
-        foreach (var v in list)
-        {
-            if (Replacer(v.Item1, v.Item2, checker))
-                return true;
-        }
-        return false;
-    }
-
-    public void Step1()
-    {
-        int m = CalcM(Word);
-
-        var vals = new List<(string, string)>
-        {
-            ("sses", "ss"), ("ies", "i"), ("ss", "ss"), ("s", "")
         };
-        ProcessSubList(vals, _ => true);
 
-        if (!Replacer("eed", "ee", x => x > 0))
+        for (int i = 0; i < suffixes.GetLength(0); i++)
         {
-            if ((VowelInStem("ed") && Replacer("ed", "", _ => true)) ||
-                (VowelInStem("ing") && Replacer("ing", "", _ => true)))
+            string suffix = suffixes[i, 0];
+            string replacement = suffixes[i, 1];
+            if (word.EndsWith(suffix))
             {
-                vals = new List<(string, string)> { ("at", "ate"), ("bl", "ble"), ("iz", "ize") };
+                string stem = word[..^suffix.Length];
+                if (MGr0.IsMatch(stem))
+                    return stem + replacement;
+            }
+        }
 
-                if (!ProcessSubList(vals, _ => true))
+        return word;
+    }
+
+    private string Step3(string word)
+    {
+        string[,] suffixes =
+        {
+            {
+                "icate", "ic"
+            },
+            {
+                "ative", ""
+            },
+            {
+                "alize", "al"
+            },
+            {
+                "iciti", "ic"
+            },
+            {
+                "ical", "ic"
+            },
+            {
+                "ful", ""
+            },
+            {
+                "ness", ""
+            }
+        };
+
+        for (int i = 0; i < suffixes.GetLength(0); i++)
+        {
+            string suffix = suffixes[i, 0];
+            string replacement = suffixes[i, 1];
+            if (word.EndsWith(suffix))
+            {
+                string stem = word[..^suffix.Length];
+                if (MGr0.IsMatch(stem))
+                    return stem + replacement;
+            }
+        }
+
+        return word;
+    }
+
+    private string Step4(string word)
+    {
+        string[] suffixes =
+        {
+            "al", "ance", "ence", "er", "ic", "able", "ible", "ant", "ement", "ment", "ent", "ion", "ou", "ism", "ate", "iti", "ous", "ive", "ize"
+        };
+
+        foreach (var suffix in suffixes)
+        {
+            if (word.EndsWith(suffix))
+            {
+                var stem = word[..^suffix.Length];
+                if (suffix == "ion")
                 {
-                    m = CalcM(Word);
-                    char last = Word[Word.Length - 1];
-
-                    if (DoubleC() && !"lsz".Contains(last))
-                    {
-                        Word = Word.Substring(0, Word.Length - 1);
-                    }
-                    else if (m == 1 && Cvc(""))
-                    {
-                        Word += "e";
-                    }
+                    if (stem.EndsWith("s") || stem.EndsWith("t"))
+                        if (MGr1.IsMatch(stem))
+                            return stem;
                 }
+                else if (MGr1.IsMatch(stem))
+                    return stem;
             }
         }
 
-        if (VowelInStem("y"))
-            Replacer("y", "i", _ => true);
+        return word;
     }
 
-    public void Step2()
+    private string Step5a(string word)
     {
-        var vals = new List<(string, string)>
+        if (word.EndsWith("e"))
         {
-            ("ational", "ate"), ("tional", "tion"), ("enci", "ence"), ("anci", "ance"),
-            ("izer", "ize"), ("bli", "ble"), ("alli", "al"), ("entli", "ent"), ("eli", "e"),
-            ("ousli", "ous"), ("ization", "ize"), ("ation", "ate"), ("ator", "ate"),
-            ("alism", "al"), ("iveness", "ive"), ("fulness", "ful"), ("ousness", "ous"),
-            ("aliti", "al"), ("iviti", "ive"), ("biliti", "ble"), ("logi", "log")
-        };
-        ProcessSubList(vals, x => x > 0);
-    }
-
-    public void Step3()
-    {
-        var vals = new List<(string, string)>
-        {
-            ("icate", "ic"), ("ative", ""), ("alize", "al"),
-            ("iciti", "ic"), ("ical", "ic"), ("ful", ""), ("ness", "")
-        };
-        ProcessSubList(vals, x => x > 0);
-    }
-
-    public void Step4()
-    {
-        var vals = new List<(string, string)>
-        {
-            ("al", ""), ("ance", ""), ("ence", ""), ("er", ""), ("ic", ""),
-            ("able", ""), ("ible", ""), ("ant", ""), ("ement", ""), ("ment", ""), ("ent", "")
-        };
-
-        bool res = ProcessSubList(vals, x => x > 1);
-
-        if (!res && Word.Length > 4)
-        {
-            if (Word[Word.Length - 4] == 's' || Word[Word.Length - 4] == 't')
-            {
-                res = Replacer("ion", "", x => x > 1);
-            }
+            var stem = word[..^1];
+            if (MGr1.IsMatch(stem) || (MEq1.IsMatch(stem) && !Cvc.IsMatch(stem)))
+                return stem;
         }
-
-        if (!res)
-        {
-            vals = new List<(string, string)>
-            {
-                ("ou", ""), ("ism", ""), ("ate", ""), ("iti", ""),
-                ("ous", ""), ("ive", ""), ("ize", "")
-            };
-            ProcessSubList(vals, x => x > 1);
-        }
+        return word;
     }
 
-    public void Step5a()
+    private string Step5b(string word)
     {
-        Replacer("e", "", x => x > 1);
-
-        if (!Cvc("e"))
-        {
-            Replacer("e", "", x => x == 1);
-        }
-    }
-
-    public void Step5b()
-    {
-        int m = CalcM(Word);
-        if (m > 1 && DoubleC() && Word.EndsWith("l"))
-        {
-            Word = Word.Substring(0, Word.Length - 1);
-        }
+        if (MGr1.IsMatch(word) && DoubleConsonant.IsMatch(word) && word.EndsWith("l"))
+            word = word[..^1];
+        return word;
     }
 }
